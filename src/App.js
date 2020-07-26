b0a24a7676cd9a8a71f60b6f33505f840a872229
joecai6 <55220898+joecai6@users.noreply.github.com>
2020-07-26 01:05:22 -0700

    added initial files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+// render home page in app
+function App() {
+  return (
+    <div className="">
+      Hello world!
+    </div>
+  );
+}
+
+export default App;
